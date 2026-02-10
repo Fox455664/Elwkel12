dba27dfc44e6324099f9f2c78f972eedda3cde7c
noorfox03-hub <noorfox03@gmail.com>
2026-02-10 14:36:12 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,12 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import path from "path";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-  server: {
-    host: "::",
-    port: 8080,
-    hmr: {
-      overlay: false,
-    },
-  },
+export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
+  // أضف هذا الجزء الخاص بالـ build
+  build: {
+    minify: false, // هذا سيمنع ضغط الكود ويظهر أسماء المتغيرات الحقيقية
+    sourcemap: true, // هذا سيساعدك في معرفة رقم السطر في الملف الأصلي
   },
-}));
+});
